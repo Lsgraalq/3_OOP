84eca6b61739a2aaa5a1fef304e34fe6acf3cad8
Lsgraalq <90512728+Lsgraalq@users.noreply.github.com>
2021-10-09 17:07:50 +0300

    Update 1 tusk

--- pervoe.py
+++ pervoe.py
@@ -1,18 +1,13 @@
 class Soda:
     """gasirovka"""
     
-    def gas(self, sirop,):
-        print("Газировка и ", sirop)
-    
-    def fake(self):
-        print("Обычная газировка")
-
-
+    def show_my_drink(self,sirop):
+        if len(sirop) == 0:
+            print("Обычная газировка")
+        else:
+            print("Газировка и ", sirop)
 def main():
     g = Soda()
     sirop = str(input("какую добаку вы хотите "))
-    if len(sirop) == 0:
-        g.fake()
-    else:
-        g.gas(sirop)
+    g.show_my_drink(sirop)
 main()
